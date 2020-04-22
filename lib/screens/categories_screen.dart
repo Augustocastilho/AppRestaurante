7e6d698469057e25c69189ef30a81f269d5ca6c9
Augusto Castilho <castilho@ice.ufjf.br>
2020-04-21 21:02:12 -0300

    Comecando a ver as listas

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 
+import '../components/category_item.dart';
+import '../data/dummy_data.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      //faz um grid com rolagem vertical
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        //limita o tamanho de 200 pra cada elemento
-        maxCrossAxisExtent: 200,
-        //define a proporção, se ele é mais largo, quadrado...
-        childAspectRatio: 3/2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos cozinhar?'),
       ),
-      children: <Widget>[
-
-      ],
+          body: GridView(
+        //faz um grid com rolagem vertical
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          //limita o tamanho de 200 pra cada elemento
+          maxCrossAxisExtent: 200,
+          //define a proporção, se ele é mais largo, quadrado...
+          childAspectRatio: 3/2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
     );
   }
 }
