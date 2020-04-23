c954741d302fa5cb875fcf46ca75f61defe70d93
Augusto Castilho <castilho@ice.ufjf.br>
2020-04-23 19:02:36 -0300

    Visualizacao das comidas nas suas categorias

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final category = ModalRoute.of(context).settings.arguments as Category ;
+    final category = ModalRoute.of(context).settings.arguments as Category;
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Receitas'),
       ),
       body: Center(
-        child: Text('Receitas por Categoria ${category.id}'),
+        child: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (ctx, index) {
+            return Text(categoryMeals[index].title);
+          },
+        ),
       ),
     );
   }
